033f73619f465acaebea976acea57a5b8e0d9e8a
Marius Barbu <msb@avengis.com>
2013-10-17 13:43:33 +0300

    force setting a run_script at deploy-time
    skip deploy on instance start; AWS calls setup->configure->deploy
    without any custom JSON

--- opsworks_nodejs/recipes/configure.rb
+++ opsworks_nodejs/recipes/configure.rb
@@ -4,6 +4,13 @@ node[:deploy].each do |application, deploy|
     next
   end
 
+  if deploy[:nodejs][:run_script] == ""
+    Chef::Log.debug("Skipping deploy::nodejs for #{application} because we have no run_script specified")
+    next
+  else
+    Chef::Log.debug("XXX Deploying the app: #{application}")
+  end
+
   template "#{deploy[:deploy_to]}/shared/config/opsworks.js" do
     cookbook 'opsworks_nodejs'
     source 'opsworks.js.erb'
