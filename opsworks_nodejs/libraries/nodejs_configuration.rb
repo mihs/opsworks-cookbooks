d8fee8f73719fc87dff980eedb31bb5cfd5ad4d3
Mihnea <mihnea@nagemus.com>
2013-10-18 15:32:15 +0300

    use environment variables when starting the node app and when running 'npm install'

--- opsworks_nodejs/libraries/nodejs_configuration.rb
+++ opsworks_nodejs/libraries/nodejs_configuration.rb
@@ -2,8 +2,9 @@ module OpsWorks
   module NodejsConfiguration
     def self.npm_install(app_name, app_config, app_root_path)
       if File.exists?("#{app_root_path}/package.json")
+        environment = app_config[:environment].to_a.map{|pair| pair.join('=')}.join(' ')
         Chef::Log.info("package.json detected. Running npm install.")
-        Chef::Log.info(OpsWorks::ShellOut.shellout("sudo su #{app_config[:user]} -c 'cd #{app_root_path} && npm install' 2>&1"))
+        Chef::Log.info(OpsWorks::ShellOut.shellout("sudo su #{app_config[:user]} -c 'cd #{app_root_path} && #{environment} npm install' 2>&1"))
       end
     end
   end
