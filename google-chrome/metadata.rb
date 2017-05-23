bd4e81c31e41e9db4e163738c920a35d8c26642f
Mihnea <mihnea@nagemus.com>
2017-05-23 15:48:44 +0300

    Remove source_url and issues_url from google-chrome cookbook metadata
    (cherry picked from commit b625279f5f86bc97e99efcf60cfdb5cdaf3ffd4b)

--- google-chrome/metadata.rb
+++ google-chrome/metadata.rb
@@ -4,8 +4,6 @@ maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
 description 'Installs/Configures Chrome browser'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url 'https://github.com/dhoer/chef-chrome'
-issues_url 'https://github.com/dhoer/chef-chrome/issues'
 version '4.0.1'
 
 chef_version '>= 12.14'
